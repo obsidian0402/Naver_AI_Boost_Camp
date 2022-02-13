357ab33c928f1fd9d3d2784cf7cd1986f01f18ab
sok chan hwi <tjrcksgnl@gmail.com>
2022-02-14 00:07:48 +0900

    no message

---
+++ WEEK3/[HW1]numpy_operation.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+art1 = np.random.rand(5, 3)
+art2 = np.random.rand(3, 2)
+
+dot = art1.dot(art2)
+
+print(dot)
---
+++ WEEK3/[HW2]concatenate.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+art1 = np.array([[5, 7], [9, 11]], float)
+art2 = np.array([[2, 4], [6, 8]], float)
+
+concat_1 = np.concatenate((art1, art2), axis=0)
+concat_2 = np.concatenate((art1, art2), axis=1)
+
+print(concat_1)
+print(concat_2)
